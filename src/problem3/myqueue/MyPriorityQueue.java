ff5f419eebf1169ec899de7d381f82708716fe94
danishali02000 <62792407+danishali02000@users.noreply.github.com>
2020-03-28 15:56:14 +0530

    Update MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,54 @@
  */
 package problem3.myqueue;
 
+
+import problem3.node.Node;
+
+import static problem1.mybst.MyBinarySearchTree.preOrder;
+import static problem1.mybst.MyBinarySearchTree.preOrderString;
+
 public class MyPriorityQueue {
-}
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyPriorityQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+    public void enqueue(Node addData) {
+        if(front == null){
+            rear = addData;
+            front = addData;
+            size++;
+        }
+        else{
+            if(addData.getData().getRollNumber() >= this.rear.getData().getRollNumber()){
+                rear.setNext(addData);
+                rear = addData;
+                size++;
+            }
+            else if(addData.getData().getRollNumber() <= this.front.getData().getRollNumber()){
+                addData.setNext(this.front);
+                this.front = addData;
+                size++;
+            }
+            else{
+                Node temp = this.front;
+                Node tempPrevious = this.front;
+                while (temp.getNext()!=null){
+                    if(temp.getData().getRollNumber() > addData.getData().getRollNumber()) {
+                       break;
+                    }
+                    tempPrevious = temp;
+                    temp = temp.getNext();
+                }
+                tempPrevious.setNext(addData);
+                addData.setNext(temp);
+                size++;
+
+            }
+        }
+    }
+
